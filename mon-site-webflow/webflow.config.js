f98f98f5b25f056bf7739525043f5f3dc3943ffb
Alexandre Teillet <alexandreteillet@Mac-mini-de-Alexandre.local>
2025-06-24 16:59:50 +0200

    update

---
+++ mon-site-webflow/webflow.config.js
@@ -0,0 +1,8 @@
+// webflow.config.js
+module.exports = {
+    host: "https://api.webflow.com",
+    rootDir: "./devlink",
+    siteId: "66c9fac9b170c5ecceb75ca1",
+    authToken: process.env.WEBFLOW_API_TOKEN,
+    cssModules: true
+  }
